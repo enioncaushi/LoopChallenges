8118ab9b272ff43024632cb26e9b7dc91ed28e26
enioncaushi <141422629+enioncaushi@users.noreply.github.com>
2023-08-08 15:10:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Print odds 1-20 
+for (var i=1; i<20; i++) {
+    if (i%2 !== 0) {
+        console.log(i);
+    }
+}
+
+// Decreasing Multiples of 3
+for (var i=100; i>=0; i-=3) {
+    console.log(i);
+}
+
+// Print the sequence
+for (var i=4; i>= -3.5; i-= 1.5) {
+    console.log(i);
+}
+
+// Sigma
+var sum=0;
+
+for ( var i=1; i<101; i++){
+    sum += i;
+}
+
+console.log(sum);
+
+// Factorial
+var product =1;
+
+for ( var i=1; i<13; i++) {
+    product *=i;
+}
+console.log(product);
